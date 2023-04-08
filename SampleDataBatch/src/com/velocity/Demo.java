2ddc1e67bd9f1eb2019c553b0716ea99761382ec
pravingiram <pravingiram7@gmail.com>
2023-04-08 23:12:53 +0530

    msg

---
+++ SampleDataBatch/src/com/velocity/Demo.java
@@ -0,0 +1,62 @@
+package com.velocity;
+
+public class Demo {
+	
+public static void main(String[] args) {
+	
+	System.out.println("this is java program");
+	
+	System.out.println("hello java");
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
