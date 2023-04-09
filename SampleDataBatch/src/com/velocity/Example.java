764fa55d9e7afd5b7a9423b5446bc57c7790bab3
pravingiram <pravingiram7@gmail.com>
2023-04-09 13:20:53 +0530

    code ended here line added

--- SampleDataBatch/src/com/velocity/Example.java
+++ SampleDataBatch/src/com/velocity/Example.java
@@ -3,7 +3,8 @@ package com.velocity;
 public class Example {
 public static void main(String[] args) {
 	for(int i=1;i<=5;i++) {
-		System.out.println(i);
+		System.out.print(i);
 	}
+	System.out.println(" "+"Code ended here");
 }
 }
